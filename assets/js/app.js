580e9d665246bcf5766a2325c0593d8be40a830d
rick <rickwestdev@gmail.com>
2019-05-14 18:35:05 +0100

    Configure Symfony Encore and set up basic styles

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
+// const $ = require('jquery');
+
+let $ = require('jquery');
+
+// Bootstrap Javascript
+import 'bootstrap';
+
+// Styles
+import '../sass/app.scss';
