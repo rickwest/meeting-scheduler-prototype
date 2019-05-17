48056c80f70198c07e27d292e9364e85e56b723c
rick <rickwestdev@gmail.com>
2019-05-17 13:56:49 +0100

    wip

--- src/Meeting/Scheduler.php
+++ src/Meeting/Scheduler.php
@@ -31,10 +31,31 @@ class Scheduler
             }
         }
 
+        // Need at least half of the non important participants to have responded before we can proceed
+        if (count($nonImportantResponses) < (count($nonImportantUsers) / 2)) {
+            return;
+        }
+
+        // Can't be scheduled because not all important users have responded.
+        // Can we do something else to resolve this conflict, for example, send reminder, offer a delegate?
         if (count($importantUsers) !== count($importantResponses)) {
             return;
         }
 
+        // Try and find a date
+
+        $preferredSlots = [];
+        $excludedSlots = [];
+
+        foreach ($meeting->getParticipantResponses() as $response) {
+            $preferredSlots = array_merge($preferredSlots, $response->getPreferenceSet());
+            $excludedSlots = array_merge($excludedSlots, $response->getExclusionSet());
+        }
+
+        // Neautral slots slpots
+        // Remove all the excluded slots
+
+        // Look across all preference sets and look for the most popular common date
         return $meeting;
     }
 }
